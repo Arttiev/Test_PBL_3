f4d0ff5cbdeadb8b5fdab72b58d176a6f0a396a6
ADMIN <ADMIN@DESKTOP-FO8QLMB>
2023-04-09 14:46:57 +0700

    Add project files.

---
+++ Test_PBL_3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_PBL_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
